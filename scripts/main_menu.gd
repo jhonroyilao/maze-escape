0750a318e839a83c931354d156d663bb5b13b1c1
jhonroyilao <jhonroyilao@gmail.com>
2026-05-28 17:20:58 +0800

    feat(menu): connect play button to redirect through loading screen

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/loadingscreen.tscn")
